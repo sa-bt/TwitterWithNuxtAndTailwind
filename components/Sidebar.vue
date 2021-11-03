422d2ddd3b749727529df5d6b6eeef86d71c3d4d
sa-bt <bakhshian2020@gmail.com>
2021-11-03 21:12:13 +0330

    implement responsive sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-1/5 border-r border-lighter px-8 py-2 flex flex-col justify-between">
+  <div class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between">
     <div>
       <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue">
         <i class="fab fa-twitter"></i>
@@ -8,30 +8,37 @@
         <button v-for="tab in tabs" @click="id = tab.id"
                 :class="`focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 ${id === tab.id ?'text-blue' : ''}`">
           <i :class="`${tab.icon } text-2xl mr-4 text-left `"></i>
-          <p class="text-lg font-semibold text-left"> {{tab.title}}</p>
+          <p class="text-lg font-semibold text-left hidden lg:block"> {{tab.title}}</p>
         </button>
       </div>
-      <button class="text-white bg-blue rounded-full font-semibold focus:outline-none w-full p-3 hover:bg-darkblue">
-        Tweet
+      <button class="text-white bg-blue rounded-full font-semibold focus:outline-none w-12 h-12 lg:w-full p-3 hover:bg-darkblue">
+        <p class="hidden lg:block">Tweet</p>
+        <i class="fas fa-plus lg:hidden"></i>
       </button>
     </div>
-    <div class="w-full relative">
-      <button class="flex items-center w-full hover:bg-lightblue rounded-full p-2 focus:outline-none">
-        <img src="favicon.ico" alt="" class="w-12 h-12 rounded-full border border-lighter">
-        <div class="ml-4">
-          <p class="text-sm font-bold leading-light">Seyed Ahmad Bakhshian</p>
-          <p class="text-sm leading-light">sa.bt@chmail.ir</p>
+    <div class="lg:w-full relative">
+      <button @click="dropdown=true" class="flex items-center w-full hover:bg-lightblue rounded-full p-2 focus:outline-none">
+        <img src="pic.jpg" alt="" class="w-12 h-12 rounded-full border border-lighter">
+        <div class="hidden lg:block ml-4">
+          <p class="text-sm font-bold leading-light">Sabt</p>
+          <p class="text-sm leading-light">@bakhshian2020</p>
         </div>
-        <i class="fas fa-angle-down ml-auto text-lg "></i>
+        <i class="hidden lg:block fas fa-angle-down ml-auto text-lg "></i>
       </button>
-      <div class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
-        <button class="flex items-center w-full hover:bg-lightest p-2 focus:outline-none">
-          <img src="favicon.ico" alt="" class="w-12 h-12 rounded-full border border-lighter">
+      <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
+        <button @click="dropdown=false" class="flex p-3 items-center w-full hover:bg-lightest p-2 focus:outline-none">
+          <img src="pic.jpg" alt="" class="w-12 h-12 rounded-full border border-lighter">
           <div class="ml-4">
-            <p class="text-sm font-bold leading-light">Seyed Ahmad Bakhshian</p>
-            <p class="text-sm leading-light">sa.bt@chmail.ir</p>
+            <p class="text-sm font-bold leading-light">Sabt</p>
+            <p class="text-sm leading-light">@bakhshian2020</p>
           </div>
-          <i class="fas fa-angle-down"></i>
+          <i class="fas fa-check ml-auto text-blue"></i>
+        </button>
+        <button class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm focus:outline-none">
+          Add an existing account
+        </button>
+        <button @click="dropdown=false" class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm">
+          Log out @bakhshian2020
         </button>
       </div>
     </div>
@@ -52,7 +59,8 @@
                     {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
                     {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'},
                 ],
-                id: 'home'
+                id: 'home',
+                dropdown:false
             }
         }
     }
